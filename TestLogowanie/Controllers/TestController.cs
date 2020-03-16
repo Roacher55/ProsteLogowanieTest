7b1581c77deed9bd31c8cf84e0fd8f49dc479358
Roacher <Roacher55@gmail.com>
2020-03-16 20:08:06 +0100

    Pierwsza wersja

---
+++ TestLogowanie/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestLogowanie.Controllers
+{
+    
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserProfile objUserLogin)
+        {
+            ZalogowanieEntities dane = new ZalogowanieEntities();
+
+            foreach (var a in dane.UserProfile)
+            {
+                if (objUserLogin.UserName == a.UserName && objUserLogin.Password == a.Password)
+                { 
+                    return View("WelcomePage", objUserLogin);
+                }
+            }
+           
+           
+                // objUserLogin.Message = "Invalid UserName/Password";
+                return View(objUserLogin);
+            
+            
+        }
+    }
+}
---
+++ TestLogowanie/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestLogowanie.MvcApplication" Language="C#" %>
